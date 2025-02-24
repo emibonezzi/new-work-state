3ebfa1582cdd00e2f37aaa71d7c5f6708f20c130
emibonezzi <emibonezzi@gmail.com>
2025-02-24 14:34:47 -0500

    Add Vacancy type definition

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Vacancy from "@/types/vacancy";
+
 export default async function Home() {
   const res = await fetch(
     `${process.env.BACKEND_URL}/api/vacancies?page=1&limit=10`
@@ -6,8 +8,8 @@ export default async function Home() {
   return (
     <div className="">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {data.vacancies.map((vacancy) => (
-          <h1>{vacancy.title}</h1>
+        {data.vacancies.map((vacancy: Vacancy) => (
+          <h1 key={vacancy.vacancy_id}>{vacancy.title}</h1>
         ))}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
