b64089ae825faddd82f10e5934416a53d1d122e5
emibonezzi <emibonezzi@gmail.com>
2025-02-25 15:23:32 -0500

    Add JobCard component

---
+++ app/components/JobCard.tsx
@@ -0,0 +1,18 @@
+import Vacancy from "@/types/vacancy";
+import React from "react";
+
+interface Props {
+  vacancy: Vacancy;
+}
+
+const JobCard = ({ vacancy }: Props) => {
+  return (
+    <article className="border border-[rgb(11_93_102)] rounded-lg p-4">
+      <span>{vacancy.date_posted}</span>
+      <h2>{vacancy.title}</h2>
+      <p>{vacancy.duties_description.slice(0, 100)}...</p>
+    </article>
+  );
+};
+
+export default JobCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Vacancy from "@/types/vacancy";
+import JobCard from "./components/JobCard";
 
 export default async function Home() {
   const res = await fetch(
@@ -7,24 +8,28 @@ export default async function Home() {
   const data = await res.json();
   return (
     <div className="m-5">
-      <header className="flex justify-between">
+      <header className="grid grid-cols-3">
         <div id="logo" className="flex items-center gap-4">
-          <img src="/new-work-state-logo.svg" alt="New Work State Logo" />
+          <img src="/new-work-state-logo.svg" />
           <h1 className="text-3xl">New Work State</h1>
         </div>
-        <div id="search-bar">
+        <div id="search-bar" className="flex items-center justify-center">
           <input
             type="text"
             placeholder="Search for jobs"
-            className="border border-gray-300 rounded-lg p-2"
+            className="border border-[rgb(11_93_102)] rounded-lg p-2"
           />
+        </div>
+        <div id="log-in" className="flex items-center justify-end gap-3">
+          <button>Sign up</button>
+          <button className="">Log In</button>
         </div>
       </header>
 
       <main className="grid grid-cols-[1fr_4fr] gap-3">
         <aside id="job-filters" className="">
           <h2>Filters</h2>
-          <div className="border border-gray-300 rounded-lg p-4">
+          <div className="border border-[rgb(11_93_102)] rounded-lg p-4">
             <h3>Location</h3>
             <ul>
               <li>New York</li>
@@ -39,49 +44,12 @@ export default async function Home() {
         </aside>
         <section id="job-dashboard">
           <h2>Jobs</h2>
-          <div className="grid grid-cols-2 gap-3">
+          <div className="grid grid-cols-[1fr_1fr_1fr] gap-3">
             {data.vacancies.map((vacancy: Vacancy) => (
-              <div
-                key={vacancy.vacancy_id}
-                className="border border-gray-300 rounded-lg p-4"
-              >
-                <h3>{vacancy.title}</h3>
-                <p>{vacancy.department}</p>
-                <p>{vacancy.county}</p>
-                <p>{vacancy.salary_range}</p>
-                <p>{vacancy.employment_type}</p>
-                <p>{vacancy.appointment_type}</p>
-                <p>{vacancy.jurisdictional_class}</p>
-                <p>{vacancy.travel_percentage}</p>
-                <p>{vacancy.workweek}</p>
-                <p>{vacancy.hours_per_week}</p>
-                <p>{vacancy.from}</p>
-                <p>{vacancy.to}</p>
-                <p>{vacancy.flextime_allowed}</p>
-                <p>{vacancy.mandatory_overtime}</p>
-                <p>{vacancy.compressed_workweek_allowed}</p>
-                <p>{vacancy.telecommuting_allowed}</p>
-                <p>{vacancy.street_address}</p>
-                <p>{vacancy.street_2}</p>
-                <p>{vacancy.city}</p>
-                <p>{vacancy.state}</p>
-                <p>{vacancy.zip_code}</p>
-                <p>{vacancy.duties_description}</p>
-                <p>{vacancy.minimum_qualifications}</p>
-                <p>{vacancy.additional_comments}</p>
-                <p>{vacancy.name}</p>
-                <p>{vacancy.telephone}</p>
-                <p>{vacancy.fax}</p>
-                <p>{vacancy.email_address}</p>
-                <p>{vacancy.street}</p>
-                <p>{vacancy.notes_on_applying}</p>
-              </div>
+              <JobCard key={vacancy._id} vacancy={vacancy}></JobCard>
             ))}
           </div>
         </section>
-        {/* {data.vacancies.map((vacancy: Vacancy) => (
-          <h1 key={vacancy.vacancy_id}>{vacancy.title}</h1>
-        ))} */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
